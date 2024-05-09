a99f73cbe8a51ebaa4f42b30839ecfe6c219ebf8
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-05-09 19:39:16 +0100

    [add] foto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,16 +9,16 @@
         <section class="container-card">
 
             <div>
-            <img class="product-levis" src="{{ $product ['frontImage'] }}" alt="immagine prodotto!">
+                <img src="{{ asset('img/' . $product['frontImage']) }}" alt="img">
             </div>
             <div class="text relaxed">
-            <span class="brand">{{ $product['brand'] }}</span>
-            <span class="hearts">&hearts;</span>
+                <span class="brand">{{ $product['brand'] }}</span>
+                <span class="hearts">&hearts;</span>
                 <div class="sconto">-50%</div>
                 <div class="sostenibilità"> Sostenibilità</div>
-            <span class="clothes-name">{{ $product['name'] }}</span>
-            <span class="solded-price">{{ $product['price'] }} &euro;</span>
-            <span class="original-price">{{ $product['brand'] }} </span>
+                    <span class="clothes-name">{{ $product['name'] }}</span>
+                    <span class="solded-price">{{ $product['price'] }} &euro;</span>
+                    <span class="original-price">{{ $product['brand'] }} </span>
             </div>
         </section>
 
